45a2b79c24607da49a36117d4a37725d47033044
nesbitr <nesbitr@bu.edu>
2015-04-22 13:06:17 -0400

    Working(One File)

---
+++ Asteroids/desktop/src/com/Asteroids/game/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.Asteroids.game.desktop;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.Asteroids.game.AsteroidsGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication((ApplicationListener) new AsteroidsGame(), config);
+	}
+}
